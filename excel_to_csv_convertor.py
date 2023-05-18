0f6fc8be38ea507e87eee58c833771ef41cf9fd6
SamithaPrabath <samithaprabath513@gmail.com>
2023-05-19 00:13:07 +0530

    chore: edit csv file

--- excel_to_csv_convertor.py
+++ excel_to_csv_convertor.py
@@ -4,3 +4,8 @@ import pandas as pd
 excel_file = pd.read_excel('cource_price.xlsx')
 
 excel_file.to_csv('course_price.csv', index=False)
+
+
+# data_url = 'https://github.com/SamithaPrabath/course-finder/blob/master/course_price.csv'
+# data = pd.read_csv(data_url)
+# print(data)
