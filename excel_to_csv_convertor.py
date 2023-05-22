d8c587a4ee8e0b638daa30461fa880ef1adc5a19
SamithaPrabath <samithaprabath513@gmail.com>
2023-05-22 19:56:59 +0530

    chore: add ipynb files

--- excel_to_csv_convertor.py
+++ excel_to_csv_convertor.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Read the Excel file
-excel_file = pd.read_excel('cource_price.xlsx')
+excel_file = pd.read_excel('Output Data set.xlsx')
 
-excel_file.to_csv('course_price.csv', index=False)
+excel_file.to_csv('test_z_core_gpa.csv', index=False)
 
 
-# data_url = 'https://github.com/SamithaPrabath/course-finder/blob/master/course_price.csv'
-# data = pd.read_csv(data_url)
+# data_url = 'https://raw.githubusercontent.com/SamithaPrabath/course-finder/master/course_price.csv'
+# data = pd.read_csv(data_url , sep=';', header=None)
 # print(data)
